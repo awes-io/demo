2503056408a6ed3c8b9e0d11b4dec2bdc9f39b71
begimov <begimov@gmail.com>
2019-04-28 11:14:45 +0300

    vitrual tour integrated

--- resources/views/sections/analytics/index.blade.php
+++ resources/views/sections/analytics/index.blade.php
@@ -9,6 +9,16 @@
 @endpush
 
 @section('content')
+    <virtual-tour name="analytics" :steps="[
+        { 
+            el: 'div.filter',
+            message: '{{ _p('pages.tour.analytics.step_1', 'You can filter information for both charts and tables.') }}'
+        },
+        { 
+            el: 'div.section',
+            message: '{{ _p('pages.tour.analytics.step_2', 'And customize their appearance and data representation.') }}'
+        },
+    ]"></virtual-tour>
     <div class="filter">
         <div class="grid grid-align-center grid-justify-between grid-justify-center--mlg">
             <div class="cell-inline">
@@ -24,36 +34,38 @@
         </div>
     </div>
 
-    @chart([
-        'default_data' => $leadsChartData,
-        'parameters' => ['is_premium', 'period'],
-        'api_url' => route('analytics.leads.chart')
-    ])
+    <div class="section">
+        @chart([
+            'default_data' => $leadsChartData,
+            'parameters' => ['is_premium', 'period'],
+            'api_url' => route('analytics.leads.chart')
+        ])
 
-    <div class="grid">
-        <div class="cell-full">
-            <div class="section">
-                @table([
-                    'name' => 'leads_table',
-                    'scope_api_url' => route('analytics.scope'),
-                    'scope_api_params' => ['is_premium', 'period']
-                ])
-                    <tb-column name="name" label="{{ _p('pages.leads.table.col.name', 'Name') }}"></tb-column>
-                    <tb-column name="email" label="{{ _p('pages.leads.table.col.email', 'Email') }}"></tb-column>
-                    <tb-column name="phone" label="{{ _p('pages.leads.table.col.phone', 'Phone') }}" media="desktop"></tb-column>
-                    <tb-column name="is_premium" label="{{ _p('pages.leads.table.col.status', 'Status') }}" media="desktop">
-                        <template slot-scope="d">
-                            <span class="status status_wait" v-if="d.data.is_premium == 1"><span>Standard</span></span>
-                            <span class="status status_inprogress" v-if="d.data.is_premium == 2"><span>Premium</span></span>
-                            <span class="status status_success" v-if="d.data.is_premium == 3"><span>Priveleged</span></span>
-                            <span class="status status_warning" v-if="d.data.is_premium == 4"><span>VIP</span></span>
-                        </template>
-                    </tb-column>
-                    <tb-column name="sales_count" label="{{ _p('pages.leads.table.col.sales', 'Sales') }}"></tb-column>
-                    @slot('mobile')
-                        <p>{{ _p('pages.leads.table.mobile.phone', 'Phone') }}: @{{ m.data.phone }}</p>
-                    @endslot
-                @endtable
+        <div class="grid">
+            <div class="cell-full">
+                <div class="section">
+                    @table([
+                        'name' => 'leads_table',
+                        'scope_api_url' => route('analytics.scope'),
+                        'scope_api_params' => ['is_premium', 'period']
+                    ])
+                        <tb-column name="name" label="{{ _p('pages.leads.table.col.name', 'Name') }}"></tb-column>
+                        <tb-column name="email" label="{{ _p('pages.leads.table.col.email', 'Email') }}"></tb-column>
+                        <tb-column name="phone" label="{{ _p('pages.leads.table.col.phone', 'Phone') }}" media="desktop"></tb-column>
+                        <tb-column name="is_premium" label="{{ _p('pages.leads.table.col.status', 'Status') }}" media="desktop">
+                            <template slot-scope="d">
+                                <span class="status status_wait" v-if="d.data.is_premium == 1"><span>Standard</span></span>
+                                <span class="status status_inprogress" v-if="d.data.is_premium == 2"><span>Premium</span></span>
+                                <span class="status status_success" v-if="d.data.is_premium == 3"><span>Priveleged</span></span>
+                                <span class="status status_warning" v-if="d.data.is_premium == 4"><span>VIP</span></span>
+                            </template>
+                        </tb-column>
+                        <tb-column name="sales_count" label="{{ _p('pages.leads.table.col.sales', 'Sales') }}"></tb-column>
+                        @slot('mobile')
+                            <p>{{ _p('pages.leads.table.mobile.phone', 'Phone') }}: @{{ m.data.phone }}</p>
+                        @endslot
+                    @endtable
+                </div>
             </div>
         </div>
     </div>
