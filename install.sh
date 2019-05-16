15230f429853a72dab85d5f0e425a5ba0adffb3e
Yevhen L <yevhen.lisovenko@awescode.de>
2019-05-16 13:53:16 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ white="\033[37m"
 
 if [ ! -e "${PWD}/.env" ]; then
     if [ -z "$1" ]; then
-        printf "$yellow |\t--------------------------------------------------------------------------------\t|$reset\n"
-        printf "$yellow |\t🙌 Follow the link to get the keys: https://www.pkgkit.com/awes-io/create 👈   \t|$reset\n"
-        printf "$yellow |\t--------------------------------------------------------------------------------\t|$reset\n\n"
+        printf "$yellow |\t-----------------------------------------------------------------------------\t|$reset\n"
+        printf "$yellow |\t🙌 Follow the link to get the keys: https://www.pkgkit.com/awes-io/create 👈 \t|$reset\n"
+        printf "$yellow |\t-----------------------------------------------------------------------------\t|$reset\n\n"
         printf "$green> Please enter your PKGKIT_CDN_KEY:$reset\n"
         read KEY
     else
